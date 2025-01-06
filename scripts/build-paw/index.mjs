bde09c55cb886f5acfed3358196b550f0777c38a
rex <rainbowmorel@163.com>
2025-01-06 22:06:53 +0800

    feat(pwa): 添加服务工作者和离线缓存支持
    - 新增 build-paw 目录，包含生成服务工作者文件的脚本
    - 添加 sw.ejs 模板文件，定义服务工作者的行为- 在 package.json 中增加 ejs 和 next-pwa 依赖
    - 更新 ESLint 配置，添加 TypeScript 相关规则

---
+++ scripts/build-paw/index.mjs
@@ -0,0 +1,49 @@
+import { readdir, stat } from 'fs/promises';
+import { fileURLToPath } from 'url';
+import { dirname, resolve, join } from 'path';
+import { readFileSync, writeFileSync } from 'fs';
+import ejs from 'ejs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const __rootname = process.cwd();
+
+// 递归遍历目录的函数
+async function traverseDir(dir) {
+  const files = [];
+
+  // 读取目录内容
+  const items = await readdir(dir);
+
+  for (const item of items) {
+    const fullPath = join(dir, item);
+
+    const stats = await stat(fullPath);
+
+    if (stats.isDirectory()) {
+      // 如果是目录,递归遍历
+      files.push(...(await traverseDir(fullPath)));
+    } else {
+      // 如果是文件,添加到结果数组
+      files.push(
+        `${fullPath.replace(__rootname, '').replace('/.next', '/next')}`
+      );
+    }
+  }
+
+  return files;
+}
+
+(async () => {
+  const staticPath = resolve(__rootname, '.next', 'static');
+  const allFiles = await traverseDir(staticPath);
+
+  const templatePath = join(__dirname, 'sw.ejs');
+  const template = readFileSync(templatePath, 'utf-8');
+
+  const renderedSw = ejs.render(template, { cacheList: allFiles });
+
+  // 写入生成的 Service Worker 文件
+  const outputPath = join(__rootname, 'public', `sw.js`);
+  console.log(outputPath);
+  writeFileSync(outputPath, renderedSw);
+})();
